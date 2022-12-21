4bde73bd0abee78f405bd1dfc01b3090e3f60ca0
miryo <miryo@vcup.moe>
2022-12-21 10:42:05 +0800

    chore: add crap trilingual

--- Resources/ExampleText.Designer.cs
+++ Resources/ExampleText.Designer.cs
@@ -21,7 +21,7 @@ namespace ResourceDemo.Resources {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class ExampleText {
+    public class ExampleText {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -35,7 +35,7 @@ namespace ResourceDemo.Resources {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ResourceDemo.Resources.ExampleText", typeof(ExampleText).Assembly);
@@ -50,12 +50,57 @@ namespace ResourceDemo.Resources {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to The demo is a example project about how to use resource file(.resx)..
+        /// </summary>
+        public static string Message_About_Demo {
+            get {
+                return ResourceManager.GetString("Message_About_Demo", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Hello!.
+        /// </summary>
+        public static string Message_Hello {
+            get {
+                return ResourceManager.GetString("Message_Hello", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Good Morafterevening！{0}！.
+        /// </summary>
+        public static string Message_HelloWithName {
+            get {
+                return ResourceManager.GetString("Message_HelloWithName", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to continue....
+        /// </summary>
+        public static string Mutual_IntoNextMessage {
+            get {
+                return ResourceManager.GetString("Mutual_IntoNextMessage", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to after into next, plz tell me u name.
+        /// </summary>
+        public static string Mutual_TellMeYourName {
+            get {
+                return ResourceManager.GetString("Mutual_TellMeYourName", resourceCulture);
             }
         }
     }
